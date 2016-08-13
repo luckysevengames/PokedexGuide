254e71eb179d8a357355338ace50f2661f1403ad
Samnang Horn <samnanghorn@samnangs-mini.local>
2016-08-13 03:54:24 -0400

    Initial Commit

---
+++ PokedexGuid-by-LuckySevenGames/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokedexGuid-by-LuckySevenGames
+//
+//  Created by Samnang Horn on 8/13/16.
+//  Copyright © 2016 LUCKY SEVEN GAMES. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
